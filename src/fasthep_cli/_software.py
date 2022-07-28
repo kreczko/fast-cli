831e396c082e3cee2ffa2e2235453b2a0f6ab425
Luke Kreczko <lkreczko@googlemail.com>
2022-07-28 14:59:22 +0100

    simplified retriaval of FAST-HEP packages and versions

--- src/fasthep_cli/_software.py
+++ src/fasthep_cli/_software.py
@@ -3,6 +3,17 @@ from __future__ import annotations
 import pkg_resources
 
 
+def _is_fasthep_package(package_name: str) -> bool:
+    """
+    Check if a package is a FAST-HEP package
+    """
+    fast_hep_prefixes = ["fasthep-", "fast-", "scikit-validate"]
+    for prefix in fast_hep_prefixes:
+        if package_name.startswith(prefix):
+            return True
+    return False
+
+
 def _find_fast_hep_packages() -> list[tuple[str, str]]:
     """
     Find all FAST-HEP packages
@@ -10,8 +21,6 @@ def _find_fast_hep_packages() -> list[tuple[str, str]]:
     fasthep_packages = {}
     installed_packages = pkg_resources.working_set
     for installed_package in installed_packages:
-        if installed_package.key.startswith(
-            "fast-"
-        ) or installed_package.key.startswith("scikit-validate"):
+        if _is_fasthep_package(installed_package.key):
             fasthep_packages[installed_package.key] = installed_package.version
     return sorted(fasthep_packages.items(), key=lambda x: (x[0], x[1]))
