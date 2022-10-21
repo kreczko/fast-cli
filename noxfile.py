33e3e07616e9cfdc4335937fda8863d0e674ec81
Luke Kreczko <lkreczko@googlemail.com>
2022-10-21 15:26:06 +0100

    test(pylint): ignoring import errors due to the nature of fasthep-cli

--- noxfile.py
+++ noxfile.py
@@ -27,7 +27,12 @@ def pylint(session: nox.Session) -> None:
     # This needs to be installed into the package environment, and is slower
     # than a pre-commit check
     session.install(".", "pylint")
-    session.run("pylint", "src", *session.posargs)
+    session.run(
+        "pylint",
+        "src",
+        "--disable=import-outside-toplevel,import-error",
+        *session.posargs,
+    )
 
 
 @nox.session
