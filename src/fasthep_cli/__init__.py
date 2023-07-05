c2beeccc759614412d144386140cb0d03b1c88e1
Luke Kreczko <lkreczko@googlemail.com>
2023-07-05 19:57:49 +0100

    release: bump version to 0.1.2

--- src/fasthep_cli/__init__.py
+++ src/fasthep_cli/__init__.py
@@ -7,6 +7,6 @@ fasthep-cli: A command line interface for the FAST-HEP tools (one to rule them a
 
 from __future__ import annotations
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = ("__version__",)
